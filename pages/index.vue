7d3468091b8f0df04e57fe3580614c8e5fd812e7
Luis Cova <luis.cova@bigbox.com.ar>
2022-04-03 22:46:35 -0300

    feat: implement content in index vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial />
+  <nuxt-content :document="doc" />
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  async asyncData({ $content }) {
+    const doc = await $content('hello').fetch()
+
+    return { doc }
+  },
 }
 </script>
